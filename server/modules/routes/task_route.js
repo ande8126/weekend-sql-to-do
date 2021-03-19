282e6753d0e5282d0ec6318ba9c67e37c03925cd
Pat Anderson <patrick.b.anderson@gmail.com>
2021-03-19 13:54:12 -0500

    have get route done on server side

--- server/modules/routes/task_route.js
+++ server/modules/routes/task_route.js
@@ -1,4 +1,4 @@
-//ROUTES HERE
+//CRUD HAPPENS HERE (ROUTES FOR GET/POST/PUT/DELETE)
 
 //bring in express
 const express = require( 'express' );
@@ -7,6 +7,20 @@ const pool = require( '../pool' );
 
 
 //routes
+router.get( '/', ( req, res )=>{
+    console.log( 'in task_route GET' );
+    let queryText = `SELECT * FROM tasks ORDER BY id;`;
+    pool.query( queryText )
+    .then( results =>{
+        //send back results from task list
+        res.send( results.rows );
+    })
+    .catch(error=>{
+        console.log( 'error getting back from tasks', error );
+        res.sendStatus( 500 );
+    })
+}) 
+
 
 //exports
 module.exports = router;
